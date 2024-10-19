e795c319ee28d318c849160a0a5302501013a78b
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-10-19 18:11:41 +0200

    ready DifferentSection section

--- src/components/DifferentBrand/DifferentBrand.jsx
+++ src/components/DifferentBrand/DifferentBrand.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { DifferentSection, List, Title } from "./DifferentBrand.styled";
+import {
+  DifferentSection,
+  Element,
+  ElementSpan,
+  Image,
+  List,
+  Paragraf,
+  Title,
+} from "./DifferentBrand.styled";
 import car from "../../Files/icons/car.png";
 import Check from "../../Files/icons/Check.png";
 import Vallet from "../../Files/icons/Vallet.png";
@@ -11,26 +19,35 @@ const DifferentBrand = () => {
       <Title>What makes our brand different</Title>
 
       <List>
-        <li>
-          <img src={car} alt="" />
-          <span></span>
-          <p></p>
-        </li>
-        <li>
-          <img src={Check} alt="" />
-          <span></span>
-          <p></p>
-        </li>
-        <li>
-          <img src={Vallet} alt="" />
-          <span></span>
-          <p></p>
-        </li>
-        <li>
-          <img src={Flower} alt="" />
-          <span></span>
-          <p></p>
-        </li>
+        <Element>
+          <Image src={car} alt="" />
+          <ElementSpan>Rychlost - náš standard</ElementSpan>
+          <Paragraf>
+            Každý recept má svůj čas přípravy, který byl ověřen našimi
+            specialisty.
+          </Paragraf>
+        </Element>
+        <Element>
+          <Image src={Check} alt="" />
+          <ElementSpan>Potvrzeno specialisty</ElementSpan>
+          <Paragraf>
+            Všechny naše recepty byly ověřeny kuchaři nejvyšší třídy
+          </Paragraf>
+        </Element>
+        <Element>
+          <Image src={Vallet} alt="" />
+          <ElementSpan>Bezkonkurenční ceny</ElementSpan>
+          <Paragraf>
+            Všechny ingredience jsou vybírány podle možností klientů.
+          </Paragraf>
+        </Element>
+        <Element>
+          <Image src={Flower} alt="" />
+          <ElementSpan>Naše zdraví je vším.</ElementSpan>
+          <Paragraf>
+            Při přípravě receptů dáváme vždy přednost zdravým potravinám.
+          </Paragraf>
+        </Element>
       </List>
     </DifferentSection>
   );
--- src/components/DifferentBrand/DifferentBrand.styled.js
+++ src/components/DifferentBrand/DifferentBrand.styled.js
@@ -26,10 +26,52 @@ export const List = styled.ul`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-around;
+
+  @media screen and (max-width: 1200px) {
+    margin: 0 auto;
+    justify-content: space-between;
+    gap: 30px;
+    width: 620px;
+  }
+  @media screen and (max-width: 767px) {
+    width: 320px;
+  }
+`;
+
+export const Element = styled.li`
+  color: var(--iq-brand-text);
+  width: 270px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 
   @media screen and (max-width: 1200px) {
   }
   @media screen and (max-width: 767px) {
     /* font-size: 20px; */
   }
+`;
+
+export const ElementSpan = styled.span`
+  font-size: 20px;
+  @media screen and (max-width: 1200px) {
+  }
+  @media screen and (max-width: 767px) {
+    font-size: 16px;
+  }
+`;
+
+export const Paragraf = styled.p`
+  font-size: 16px;
+
+  @media screen and (max-width: 767px) {
+    font-size: 14px;
+  }
+`;
+
+export const Image = styled.img`
+  width: 24px;
+  margin-bottom: 18px;
 `;
