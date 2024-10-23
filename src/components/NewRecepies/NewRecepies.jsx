1cd3e1f3d14fd29d544fe0b81a546e65f21ba4dc
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-10-23 18:38:06 +0200

    create card component

--- src/components/NewRecepies/NewRecepies.jsx
+++ src/components/NewRecepies/NewRecepies.jsx
@@ -2,14 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { SectionNewRecepies } from "./NewRecepies.styled";
 import { useSelector } from "react-redux";
+import OneRecipe from "../OneRecipe/OneRecipe";
 
 const NewRecepies = () => {
   const products = useSelector((state) => state.products.list);
-  console.log(products.recipes);
+  //   console.log(products.recipes);
+  function mostFamous(products) {
+    if (!products || !products.recipes) return [];
+    console.log(products);
+    return products.recipes.filter((item) => item.rating === 4.9);
+  }
+  const popularRecipes = mostFamous(products);
+  console.log(popularRecipes);
+  //   console.log(mostFamous(products).map((el) => console.log(el)));
   return (
     <SectionNewRecepies>
-      <h2>NewRecepies</h2>
-      <div>photo</div>
+      <h2>Our most popular recipes</h2>
+      <div>
+        {popularRecipes.map((recipe) => (
+          <OneRecipe recipe={recipe} />
+          //   <div key={recipe.key}>{recipe.rating}</div>
+        ))}
+      </div>
       <Link rel="#" href="#" />
     </SectionNewRecepies>
   );
---
+++ src/components/OneRecipe/OneRecipe.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const OneRecipe = ({ recipe }) => {
+  console.log(recipe);
+  return (
+    <div>
+      <img src="" alt="" />
+      <span>name</span>
+      <p>shrehe5 e5tgw45yw 5etw54w54g we5gw4egw4g eg54g</p>
+      <div>
+        <div>
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
+        <div>
+          <svg />
+          <span>??? min</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OneRecipe;
