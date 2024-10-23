a1084ccc90d0ba89fecf473e41b87f9bbd0bd77f
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-10-23 19:42:36 +0200

    add styles

--- src/components/OneRecipe/OneRecipe.jsx
+++ src/components/OneRecipe/OneRecipe.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { ImgRecipe, OneItem } from "./OneRecipe.styled";
 
 const OneRecipe = ({ recipe }) => {
   console.log(recipe);
   return (
-    <div>
-      <img src="" alt="" />
+    <OneItem>
+      <ImgRecipe src={recipe.image} alt="" />
       <span>name</span>
       <p>shrehe5 e5tgw45yw 5etw54w54g we5gw4egw4g eg54g</p>
       <div>
@@ -14,11 +15,11 @@ const OneRecipe = ({ recipe }) => {
           <img src="" alt="" />
         </div>
         <div>
-          <svg />
-          <span>??? min</span>
+          {/* <svg />
+          <span>??? min</span> */}
         </div>
       </div>
-    </div>
+    </OneItem>
   );
 };
 
---
+++ src/components/OneRecipe/OneRecipe.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const OneItem = styled.div`
+  width: 195px;
+`;
+
+export const ImgRecipe = styled.img`
+  /* width: 195px; */
+  height: 250px;
+  border-radius: 10px;
+`;
