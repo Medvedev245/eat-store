13687f7d0e5e6bd47011530dfce2327c76ee208c
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2024-12-14 16:49:37 +0100

    add block JoinBlock component - control mistake

--- src/components/JoinBlock/JoinBlock.jsx
+++ src/components/JoinBlock/JoinBlock.jsx
@@ -1,61 +1,61 @@
-// import React, { useState } from "react";
-// import { Notify } from "notiflix/build/notiflix-notify-aio";
-// import {
-//   FormWrap,
-//   InputField,
-//   MainContainer,
-//   SectionJoinBlock,
-//   SubmitButton,
-//   WrapMainText,
-//   WrapText,
-// } from "./JoinBlock.styled";
+import React, { useState } from "react";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+import {
+  FormWrap,
+  InputField,
+  MainContainer,
+  SectionJoinBlock,
+  SubmitButton,
+  WrapMainText,
+  WrapText,
+} from "./JoinBlock.styled";
 
-// const JoinBlock = () => {
-//   const [email, setEmail] = useState("");
-//   const [isSubmitting, setIsSubmitting] = useState(false);
+const JoinBlock = () => {
+  const [email, setEmail] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     let email = event.target[0].value;
-//     setIsSubmitting(true);
-//     console.log(email);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let email = event.target[0].value;
+    setIsSubmitting(true);
+    console.log(email);
 
-//     setTimeout(() => {
-//       setIsSubmitting(false);
-//       setEmail("");
-//       event.target.reset();
-//       Notify.success("Zpráva byla odeslána");
-//     }, 1000);
-//   };
+    setTimeout(() => {
+      setIsSubmitting(false);
+      setEmail("");
+      event.target.reset();
+      Notify.success("Zpráva byla odeslána");
+    }, 1000);
+  };
 
-//   const handleChange = (event) => {
-//     setEmail(event.target.value);
-//   };
+  const handleChange = (event) => {
+    setEmail(event.target.value);
+  };
 
-//   return (
-//     <SectionJoinBlock>
-//       <MainContainer>
-//         <WrapMainText>
-//           Přihlaste se k odběru a dostávejte nové recepty na e-mail.
-//         </WrapMainText>
-//         <WrapText>
-//           Přihlaste se k odběru týdenních nových receptů od předních odborníků
-//         </WrapText>
-//         <FormWrap onSubmit={handleSubmit}>
-//           <InputField
-//             type="email"
-//             placeholder="Zadejte váš e-mail"
-//             value={email}
-//             onChange={handleChange}
-//             disabled={isSubmitting}
-//           />
-//           <SubmitButton type="submit" disabled={isSubmitting}>
-//             {isSubmitting ? "Odesílání..." : "Přihlásit se"}
-//           </SubmitButton>
-//         </FormWrap>
-//       </MainContainer>
-//     </SectionJoinBlock>
-//   );
-// };
+  return (
+    <SectionJoinBlock>
+      <MainContainer>
+        <WrapMainText>
+          Přihlaste se k odběru a dostávejte nové recepty na e-mail.
+        </WrapMainText>
+        <WrapText>
+          Přihlaste se k odběru týdenních nových receptů od předních odborníků
+        </WrapText>
+        <FormWrap onSubmit={handleSubmit}>
+          <InputField
+            type="email"
+            placeholder="Zadejte váš e-mail"
+            value={email}
+            onChange={handleChange}
+            disabled={isSubmitting}
+          />
+          <SubmitButton type="submit" disabled={isSubmitting}>
+            {isSubmitting ? "Odesílání..." : "Přihlásit se"}
+          </SubmitButton>
+        </FormWrap>
+      </MainContainer>
+    </SectionJoinBlock>
+  );
+};
 
-// export default JoinBlock;
+export default JoinBlock;
